224381251bd702f5d952950b187ed3303605dadf
ronikita <rooleg@mail.ru>
2018-04-26 14:23:30 +0300

    ...

--- 1.py
+++ 1.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3 
-def DoTwo(num1,num2):
-	print ( "%d + %d = %d" % (num1,num2, num1+num2)) #Lol
-def DoThree(num11,num22,num33):
-	a=b+c
-	b=c+d
-def DoFour():
-	print("Boris")
+
 #первая строка указывает что нужно компилировать при помощи интерпритатора питон3
 print ("Hello World!!!")
 name = input("What is your name?\n")
 print ("HELLO there, " + name + "!")
 strng = 'Nikita'
-print(strng*2) 
+print(strng) 
 print("printf")
 var =2+1
 print(type(var))
 print(var)
 	#privet
 print(str(var)*5) 
+a=3
+b=4
+c=1
+d=4
+e=3
 #внутри этого принта мы сделали var типа char после выхода изменения не сохраняются
 for i in range(1,3):
 	print("new print:   ", i)
@@ -29,7 +28,7 @@ print (month[0:2])
 '''
 цикл идет по вызовам пока перед ними есть табуляции или пробелы
 '''
-while 1>2:
+while 1+3>2:
 	a=b+c
 	while 2>3:
 		b=e+c
@@ -51,9 +50,6 @@ mas1=mas1+mas2
 print(mas1)
 print(type(mas2))
 print (type(month))
-DoTwo(1,2)
-DoFour()
-DoThree(0,17,1)
 '''
 будет реализован:
 массив(список)
